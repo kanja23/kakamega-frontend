880c56489d6204164208df1481bca44110e48ef5
Martin Karanja <martinkaranja92@gmail.com>
2025-09-05 21:28:09 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // This line should exist
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
