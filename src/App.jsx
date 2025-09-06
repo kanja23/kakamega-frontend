7d05a6dba7b95010f042fc9094d21ccaf6de82c3
Martin Karanja <martinkaranja92@gmail.com>
2025-09-06 13:41:53 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './LoginPage.jsx'
 import DashboardPage from './DashboardPage.jsx'
+import MeterInspectionPage from './MeterInspectionPage.jsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/meter-inspection" element={<MeterInspectionPage />} />
       </Routes>
     </BrowserRouter>
   )
