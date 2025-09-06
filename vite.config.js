a4d1499a853a3d44ced9d25f07897cca91eea691
Martin Karanja <martinkaranja92@gmail.com>
2025-09-06 17:16:01 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     rollupOptions: {
       external: ['@emailjs/browser'] // Exclude @emailjs/browser from bundling
     }
