855e70081da34934db869f95d3517dd5121e3012
Martin Karanja <martinkaranja92@gmail.com>
2025-09-07 12:15:45 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Explicitly set root to current directory
   base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
       external: ['@emailjs/browser'] // External due to CDN
+    }
+  },
+  server: {
+    fs: {
+      allow: ['.'] // Allow serving files from root
     }
   }
 });
