e0098b7943db70018a453fae75a9f095e1c80e7d
Martin Karanja <martinkaranja92@gmail.com>
2025-09-07 22:44:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './LoginPage.jsx'
 import DashboardPage from './DashboardPage.jsx'
 import MeterInspectionPage from './MeterInspectionPage.jsx'
-import ReportsPage from './ReportsPage.jsx' // Add this import
+import ReportsPage from './ReportsPage.jsx'
+import OutagePage from './OutagePage.jsx' // Add this import
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/meter-inspection" element={<MeterInspectionPage />} />
-        <Route path="/reports" element={<ReportsPage />} /> {/* Add this route */}
+        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="/outage-reporting" element={<OutagePage />} /> {/* Add this route */}
       </Routes>
     </BrowserRouter>
   )
