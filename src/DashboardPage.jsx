29b76165d94df3983c13cce7f210ceb4900f17bc
Martin Karanja <martinkaranja92@gmail.com>
2025-09-08 09:07:19 +0300

    Update DashboardPage.jsx

--- src/DashboardPage.jsx
+++ src/DashboardPage.jsx
@@ -26,6 +26,13 @@ const MeterInspectionIcon = () => (
   </svg>
 );
 
+// Add this icon component with the other icons
+const DisconnectionsIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="currentColor" d="M21 10.12h-6.78l2.74-2.82c-2.73-2.7-7.15-2.8-9.88-.1-2.73 2.71-2.73 7.08 0 9.79 2.73 2.71 7.15 2.71 9.88 0C18.32 15.65 19 14.08 19 12.1h2c0 1.98-.88 4.55-2.64 6.29-3.51 3.48-9.21 3.48-12.72 0-3.5-3.47-3.53-9.11-.02-12.58 3.51-3.47 9.14-3.47 12.65 0L21 3v7.12zM12.5 8v4.25l3.5 2.08-.72 1.21L11 13V8h1.5z"/>
+  </svg>
+);
+
 const ReportOutageIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
     <path fill="currentColor" d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"/>
@@ -141,6 +148,13 @@ function DashboardPage() {
               label="Report Outage" 
               color="#ff6633"
               onClick={() => navigate('/outage-reporting')}
+            />
+            // Add this to the actions grid
+            <GridIcon 
+              iconSvg={<DisconnectionsIcon />}
+              label="Disconnections" 
+              color="#cc3333"
+              onClick={() => navigate('/disconnections')}
             />
             <GridIcon 
               iconSvg={<AnomaliesIcon />}
